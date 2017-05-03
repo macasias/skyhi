2a1b58e5de6116a295d8d3f38140514ead4a5f27
macasias <macasias@ucdavis.edu>
2017-05-02 23:09:14 -0700

    Added all files: Made to login screen

---
+++ app/src/main/java/com/example/skyhi/LoginScreen.java
@@ -0,0 +1,16 @@
+package com.example.skyhi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+        Intent intent = getIntent();
+    }
+}
---
+++ app/src/main/java/com/example/skyhi/SkyHiHome.java
@@ -0,0 +1,36 @@
+package com.example.skyhi;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SkyHiHome extends AppCompatActivity {
+    //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sky_hi_home);
+        EditText Ed1 = (EditText)findViewById(R.id.phon3_number);
+        disableEditText(Ed1);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginScreen.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    private void disableEditText(EditText editText) {
+        editText.setFocusable(false);
+        editText.setEnabled(false);
+        editText.setCursorVisible(false);
+        editText.setKeyListener(null);
+        editText.setBackgroundColor(Color.TRANSPARENT);
+    }
+}
