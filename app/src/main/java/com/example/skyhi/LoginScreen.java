b608c0255c38625ee2f7361f150909c170196b61
macasias <macasias@ucdavis.edu>
2017-05-04 22:07:52 -0700

    Troubleshoot screen opens again

--- app/src/main/java/com/example/skyhi/LoginScreen.java
+++ app/src/main/java/com/example/skyhi/LoginScreen.java
@@ -10,6 +10,7 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        setTitle("Login");
 
         Intent intent = getIntent();
     }
--- app/src/main/java/com/example/skyhi/SkyHiHome.java
+++ app/src/main/java/com/example/skyhi/SkyHiHome.java
@@ -2,10 +2,13 @@ package com.example.skyhi;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import static java.net.Proxy.Type.HTTP;
 
 public class SkyHiHome extends AppCompatActivity {
     //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -14,16 +17,36 @@ public class SkyHiHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sky_hi_home);
-        EditText Ed1 = (EditText)findViewById(R.id.phon3_number);
+        setTitle("Welcome!");
+        EditText Ed1 = (EditText)findViewById(R.id.contact_message);
+        EditText Ed2 = (EditText)findViewById(R.id.login_message);
         disableEditText(Ed1);
+        disableEditText(Ed2);
     }
 
     public void goToLogin(View view) {
         Intent intent = new Intent(this, LoginScreen.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void goToTroubleshootScreen(View view) {
+        Intent intent = new Intent(this, TroubleshootScreen.class);
+        startActivity(intent);
+    }
+
+    public void callSkyHi(View view) {
+        Uri number = Uri.parse("tel:5555555555");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+        startActivity(callIntent);
+    }
+
+    public void emailSkyhi(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"admin@skyhibroadband.com"}); // recipients
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Trouble Shooting");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "I don't fucking know what to put here");
+        startActivity(emailIntent);
     }
 
     private void disableEditText(EditText editText) {
